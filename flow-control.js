660005869034fdde0145b1abba83653b28cfcd41
Oleksandr <bilovka10@gmail.com>
2020-08-06 21:08:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -36,6 +36,7 @@ function switchAge(age) {
     case age>=13&&age<=19 : console.log ("You are a teenager") 
     break;
     case age<13&&age>19 : console.log("You have an age")
+    break;
     default: return "You have an age"
   }
 }
